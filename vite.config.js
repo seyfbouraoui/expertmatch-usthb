ffb7136edc659bcc583b96ceb8f29b6781a61031
seyfbouraoui <seyfbouraoui@gmail.com>
2026-05-23 14:49:38 +0200

    Fix vite.config.js conflict

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-<<<<<<< HEAD
 export default defineConfig({
   plugins: [react()],
   base: '/expertmatch-usthb/',
 })
-=======
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
->>>>>>> 941292f347d6de74011e52a3ce9ba4e759b27926
